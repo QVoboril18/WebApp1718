0912cb6a9f6e081a684e53b1b52e7fbd98b956ff
Aiden Woodard <awoodard19@slp-awoodard19.local>
2017-12-15 10:04:24 -0700

    try this

--- website/routes/index.js
+++ website/routes/index.js
@@ -17,8 +17,8 @@ var TeacherLogins = [["lhaynes", "lhaynesPW"], ["egaffney", "egaffneyPW"]];
 var StudentLogins = [["qvoboril", "qvoborilPW"], ["awoodard", "awoodardPW"]]
 
 var userLoginSchema = new Schema({
-    username: String,
-    password: String,
+    username: req.body.username,
+    password: req.body.password
 }, {collection: 'user-login'});
 
 var UserLogin = mongoose.model('UserLogin', userLoginSchema);
