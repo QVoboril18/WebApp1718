6980f2d72567903ddd2b4b538074700cfc01d0a6
Aiden Woodard <awoodard19@slp-awoodard19.local>
2017-12-15 09:16:17 -0700

    Throw in requirements

--- website/routes/index.js
+++ website/routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+mongoose.connect('localhost:27017/test');
+var Schema = mongoose.Schema;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
