45b66e9f0641d9360e7b47c43da4dc6ac615bf08
Aiden Woodard <awoodard19@slp-awoodard19.local>
2017-12-15 09:33:36 -0700

    Username and password stuffs

--- website/routes/index.js
+++ website/routes/index.js
@@ -9,4 +9,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'KDS TurnItIn' });
 });
 
+var TeacherLogins = [[lhaynes, lhaynesPW], [egaffney, egaffneyPW] ];
+var StudentLogins = [[qvoboril, qvoborilPW], [awoodard, awoodardPW]]
+
+var userLoginSchema = new Schema({
+    username: String,
+    password: String,
+}, {collection: 'user-login'});
+
+var UserLogin = mongoose.model('UserLogin', userLoginSchema);
+
+router.get('/get-data', function(req, res, next) {
+  if(TeacherLogins.contains([UserLogin.find(username), UserLogin.find(password)])){
+    res.render('teacher', {username: UserLogin.find(username)});
+  }
+  else if(StudentLogins.contains([UserLogin.find(username), UserLogin.find(password)])){
+        res.render('student', {username: UserLogin.find(username)});
+    }
+
+
+    // UserData.find()
+    //     .then(function(doc) {
+    //         res.render('', {items: doc});
+    //     });
+});
+
+
+
+
 module.exports = router;
